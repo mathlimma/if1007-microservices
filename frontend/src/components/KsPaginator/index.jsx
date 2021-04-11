1ad7df053dea0411a271f5288c1c3ead2c03b66f
Geraldo Galvao <gmgn@cin.ufpe.br>
2021-04-11 15:08:33 -0300

    Add paginator to Share page

---
+++ frontend/src/components/KsPaginator/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
+const KsPaginator = ({ pagesNumber, currentPage }) => {
+  const renderPageNumbers = () => Array.from(Array(pagesNumber).keys())
+    .map((pageNumber) => (
+      <PaginationItem active={currentPage === pageNumber + 1}>
+        <PaginationLink href="#">
+          {pageNumber + 1}
+        </PaginationLink>
+      </PaginationItem>
+    ));
+
+  return (
+    <div className="d-flex justify-content-center">
+      <Pagination>
+        <PaginationItem disabled={currentPage === 1}>
+          <PaginationLink first href="#" />
+        </PaginationItem>
+        <PaginationItem disabled={currentPage === 1}>
+          <PaginationLink previous href="#" />
+        </PaginationItem>
+        {renderPageNumbers()}
+        <PaginationItem disabled={currentPage === pagesNumber}>
+          <PaginationLink next href="#" />
+        </PaginationItem>
+        <PaginationItem disabled={currentPage === pagesNumber} >
+          <PaginationLink last href="#" />
+        </PaginationItem>
+      </Pagination>
+    </div >
+  );
+};
+
+KsPaginator.propTypes = {
+  pagesNumber: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+};
+
+export default KsPaginator;
--- frontend/src/pages/SharePage/index.jsx
+++ frontend/src/pages/SharePage/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import KsKitGrid from "../../components/KsKitGrid";
 import dataMock from "../../dataMock";
+import KsPaginator from '../../components/KsPaginator';
 
 const SharePage = () => (
   <div className="container">
     <KsKitGrid data={dataMock.content} />
+    <KsPaginator pagesNumber={1} currentPage={1} />
   </div>
 );
 
