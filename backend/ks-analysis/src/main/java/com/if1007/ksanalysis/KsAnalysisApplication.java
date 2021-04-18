4fc80b00c9eb609a40a052590bab7b77367c6233
mathlimma <mal3@cin.ufpe.br>
2021-04-18 16:53:01 -0300

    feat: change favicon

---
+++ backend/ks-analysis/src/main/java/com/if1007/ksanalysis/KsAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.if1007.ksanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsAnalysisApplication.class, args);
+	}
+
+}
---
+++ backend/ks-analysis/src/test/java/com/if1007/ksanalysis/KsAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.if1007.ksanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
