5632067f9b038c23777e4ceef57d120f1b3b130a
hitallocavas <hcs3@cin.ufpe.br>
2021-04-26 21:09:12 -0300

    Ajustes de cors pra preflight

--- backend/ks-core/src/main/java/com/if1007/kscore/config/WebConfig.java
+++ backend/ks-core/src/main/java/com/if1007/kscore/config/WebConfig.java
@@ -8,9 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
     }
 }
